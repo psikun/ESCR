164186bffc446602964f4883358aba6fc76b8620
psikun <psikun@foxmail.com>
2023-02-19 11:50:23 +0800

    feat: 搭建项目整体依赖

--- gateway/src/main/java/com/escr/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/escr/gateway/GatewayApplication.java
@@ -2,11 +2,13 @@ package com.escr.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author psikun
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication {
 
     public static void main(String[] args) {
