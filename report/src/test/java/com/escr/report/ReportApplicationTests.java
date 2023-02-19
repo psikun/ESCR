e22f7cc470fc33b9a7904f314f7a0ef7473c420e
psikun <psikun@foxmail.com>
2023-02-19 11:04:26 +0800

    feat: 引入report模块

---
+++ report/src/test/java/com/escr/report/ReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escr.report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
