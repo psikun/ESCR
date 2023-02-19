a5612c64d47f4c1da8f81927619c0d6dde2abd11
psikun <psikun@foxmail.com>
2023-02-19 19:30:22 +0800

    refactor: 修改auth模块

---
+++ auth/src/main/java/com/escr/auth/AuthApplication.java
@@ -0,0 +1,16 @@
+package com.escr.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author psikun
+ */
+@SpringBootApplication
+public class AuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApplication.class, args);
+	}
+
+}
