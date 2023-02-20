29090c4b96f888c1a34039115812831567c5d54f
psikun <psikun@foxmail.com>
2023-02-20 15:47:43 +0800

    feat: 整合Redis数据库

--- auth/src/main/java/com/escr/auth/AuthApplication.java
+++ auth/src/main/java/com/escr/auth/AuthApplication.java
@@ -1,16 +1,21 @@
 package com.escr.auth;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
+ * The type Auth application.
+ *
  * @author psikun
  */
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.escr.auth", "com.escr.common"})
+@MapperScan("com.escr.auth.mapper")
 public class AuthApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
 
 }
