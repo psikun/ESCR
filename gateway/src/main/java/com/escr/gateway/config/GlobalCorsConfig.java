880a285c2149bad7d123e5c0eec7a174f14c39ec
psikun <psikun@foxmail.com>
2023-04-14 18:51:08 +0800

    feat: 新增全局跨域配置

---
+++ gateway/src/main/java/com/escr/gateway/config/GlobalCorsConfig.java
@@ -0,0 +1,30 @@
+package com.escr.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.util.pattern.PathPatternParser;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * @author psikun
+ * @description 全局跨域配置,前端从网关进行调用时需要配置
+ * @date 2023/04/14/ 18:48
+ */
+
+@Configuration
+public class GlobalCorsConfig {
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOriginPattern("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
+    }
+
+}
